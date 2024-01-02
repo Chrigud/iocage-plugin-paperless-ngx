71c5e3a296d7389f1053b3fdee55fc926e3e0c1d
Chris Gude <github@gude.it>
2024-01-02 09:30:11 +0100

    Add initial iocage plugin for paperless-ngx

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iocage-plugin-paperless-ngx
-Artifacts file(s) for Paperless-ngx iocage plugin
+Artifact file(s) for Paperless NGX iocage plugin
+
+
+After install, the admin username and password are both "admin".  Please change at first login for security reasons.  The web portal for paperless-ngx will be the jail ip address and port 8000.  More information on paperless-ngx can be found at https://paperless-ngx.readthedocs.io/en/latest/index.html.
---
+++ post_install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+
+sysrc -f /etc/rc.conf redis_enable="YES"
+service redis start
+
+
+
+mkdir /opt
+curl -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v2.2.1/paperless-ngx-v2.2.1.tar.xz --output paperless-ngx-v2.2.1.tar.xz
+tar -zxf paperless-ngx-v2.2.1.tar.xz
+mv paperless-ngx /opt/paperless
+pw user add -n paperless -c 'Paperless' -d /opt/paperless -m -s /bin/sh
+cd /opt/paperless
+chown -R paperless:paperless /opt/paperless
+
+
+sed -i "" -e 's/#PAPERLESS_CONSUMER_POLLING/PAPERLESS_CONSUMER_POLLING/' /opt/paperless/paperless.conf
+sed -i "" -e 's/#PAPERLESS_DATA_DIR/PAPERLESS_DATA_DIR/' /opt/paperless/paperless.conf
+sed -i "" -e  "/PAPERLESS_DATA_DIR/ a\\
+PAPERLESS_NLTK_DIR=../data/nltk\
+" /opt/paperless/paperless.conf
+sed -i "" -e 's/#PAPERLESS_MEDIA_ROOT/PAPERLESS_MEDIA_ROOT/' /opt/paperless/paperless.conf
+sed -i "" -e 's/#PAPERLESS_CONSUMPTION_DIR/PAPERLESS_CONSUMPTION_DIR/' /opt/paperless/paperless.conf
+sed -i "" -e 's/#PAPERLESS_REDIS/PAPERLESS_REDIS/' /opt/paperless/paperless.conf
+sed -i "" -e  "/PAPERLESS_REDIS/ a\\
+PAPERLESS_DBENGINE=sqlite\
+" /opt/paperless/paperless.conf
+
+
+sed -i "" -e '/PDF/s/rights="none"/rights="read|write"/' /usr/local/etc/ImageMagick-7/policy.xml
+
+
+su paperless -c /tmp/paperless_install
+sysrc -f /etc/rc.conf paperlessconsumer_enable="YES"
+sysrc -f /etc/rc.conf paperlesswebserver_enable="YES"
+sysrc -f /etc/rc.conf paperlessscheduler_enable="YES"
+sysrc -f /etc/rc.conf paperlesstaskqueue_enable="YES"
+service paperlesswebserver start
+service paperlessconsumer start
+service paperlessscheduler start
+service paperlesstaskqueue start
+
+echo "The default username and password for this install is admin for both" >> /root/PLUGIN_INFO
